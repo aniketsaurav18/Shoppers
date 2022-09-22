11e4eb6e0500da4760c2ebd052035325d4ed0182
Aniket <aniketsaurav18@gmail.com>
2022-09-23 02:15:42 +0530

    chapter 7 completed

--- models/product.js
+++ models/product.js
@@ -1,5 +1,21 @@
 const fs = require('fs');
 const path = require('path');
+const p = path.join(
+  path.dirname(process.mainModule.filename),
+  "data",
+  "products.json"
+);
+const getProductsFromFile = cb =>{
+  
+  fs.readFile(p, (err, fileContent) => {
+    if (err) {
+      cb([]);
+    }else{
+      cb(JSON.parse(fileContent));
+    }
+    
+  });
+}
 
 module.exports = class Product {
   constructor(t) {
@@ -7,34 +23,16 @@ module.exports = class Product {
   }
 
   save() {
-    const p = path.join(
-      path.dirname(process.mainModule.filename),
-      'data',
-      'products.json'
-    );
-    fs.readFile(p, (err, fileContent) => {
-      let products = [];
-      if (!err) {
-        products = JSON.parse(fileContent);
-      }
+    getProductsFromFile(products =>{
       products.push(this);
-      fs.writeFile(p, JSON.stringify(products), err => {
+      fs.writeFile(p, JSON.stringify(products), (err) => {
         console.log(err);
       });
     });
+    
   }
 
   static fetchAll(cb) {
-    const p = path.join(
-      path.dirname(process.mainModule.filename),
-      'data',
-      'products.json'
-    );
-    fs.readFile(p, (err, fileContent) => {
-      if (err) {
-        cb([]);
-      }
-      cb(JSON.parse(fileContent));
-    });
+    getProductsFromFile(cb);
   }
 };
