cc086561fe9b5bdeb548581380ef4f87e6232dba
Aniket <aniketsaurav18@gmail.com>
2023-01-09 19:57:40 +0530

    module 10 complete

--- models/product.js
+++ models/product.js
@@ -1,6 +1,6 @@
-const db = require('../util/database');
+const db = require("../util/database");
 
-const Cart = require('./cart');
+const Cart = require("./cart");
 
 module.exports = class Product {
   constructor(id, title, imageUrl, description, price) {
@@ -12,18 +12,19 @@ module.exports = class Product {
   }
 
   save() {
-   
+    return db.execute(
+      "INSERT INTO products (title,price,imageUrl,description) VALUES (?,?,?,?)",
+      [this.title, this.price, this.imageUrl, this.description]
+    );
   }
 
-  static deleteById(id) {
-    
-  }
+  static deleteById(id) {}
 
   static fetchAll() {
-    return db.execute('SELECT * FROM products');
+    return db.execute("SELECT * FROM products");
   }
 
   static findById(id) {
-    
+    return db.execute("SELECT * FROM products WHERE products.id=?", [id]);
   }
 };
