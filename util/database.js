c4de41b0c834a73d99dbe826fb8e38af475d8649
Aniket <aniketsaurav18@gmail.com>
2023-01-23 01:18:27 +0530

    mongodb second commit

--- util/database.js
+++ util/database.js
@@ -1,8 +1,17 @@
-const Sequelize = require("sequelize");
+const mongodb = require("mongodb");
+const MongoClient = mongodb.MongoClient;
 
-const sequelize = new Sequelize("node-complete", "root", "123456", {
-  dialect: "mysql",
-  host: "localhost",
-});
+const mongoConnect = (callback) => {
+  MongoClient.connect(
+    "mongodb+srv://aniketsaurav:Aniket%232486@cluster0.xxs8c7n.mongodb.net/?retryWrites=true&w=majority"
+  )
+    .then((client) => {
+      console.log("Database Connected");
+      callback(client);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
 
-module.exports = sequelize;
+module.exports = mongoConnect;
